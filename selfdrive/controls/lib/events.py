48564ab113296536d769a4a0b02771f5ef6ab96f
cfranhonda <46506059+cfranhonda@users.noreply.github.com>
2020-11-25 08:28:37 -0800

    Longer to fix sounds

--- selfdrive/controls/lib/events.py
+++ selfdrive/controls/lib/events.py
@@ -143,7 +143,7 @@ class NoEntryAlert(Alert):
                visual_alert=VisualAlert.none, duration_hud_alert=2.):
     super().__init__("openpilot Unavailable", alert_text_2, AlertStatus.normal,
                      AlertSize.mid, Priority.LOW, visual_alert,
-                     audible_alert, .4, duration_hud_alert, 3.)
+                     audible_alert, 1.0, duration_hud_alert, 3.)
 
 
 class SoftDisableAlert(Alert):
@@ -151,7 +151,7 @@ class SoftDisableAlert(Alert):
     super().__init__("TAKE CONTROL IMMEDIATELY", alert_text_2,
                      AlertStatus.critical, AlertSize.full,
                      Priority.MID, VisualAlert.steerRequired,
-                     AudibleAlert.chimeWarningRepeat, .1, 2., 2.),
+                     AudibleAlert.chimeWarningRepeat, 1.0, 2., 2.),
 
 
 class ImmediateDisableAlert(Alert):
@@ -166,7 +166,7 @@ class EngagementAlert(Alert):
     super().__init__("", "",
                      AlertStatus.normal, AlertSize.none,
                      Priority.MID, VisualAlert.none,
-                     audible_alert, .2, 0., 0.),
+                     audible_alert, 1.0, 0., 0.),
 
 class NormalPermanentAlert(Alert):
   def __init__(self, alert_text_1, alert_text_2):
