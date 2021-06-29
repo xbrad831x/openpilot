980b25ec521bdca47aab6b723728cc35e38ba61e
wirelessnet2 <37725998+wirelessnet2@users.noreply.github.com>
2021-06-28 20:17:54 -0400

    LKAS Disable Module
    LKAS Disable Module
    
    Update car.capnp
    
    Update car.capnp
    
    Update carstate.py
    
    Update car.capnp
    
    Update carcontroller.py
    
    Update carcontroller.py

--- selfdrive/car/honda/carcontroller.py
+++ selfdrive/car/honda/carcontroller.py
@@ -93,8 +93,7 @@ def process_hud_alert(hud_alert):
 
 HUDData = namedtuple("HUDData",
                      ["pcm_accel", "v_cruise", "car",
-                     "lanes", "fcw", "acc_alert", "steer_required"])
-
+                     "lanes", "fcw", "acc_alert", "steer_required", "dashed_lanes"])
 
 class CarController():
   def __init__(self, dbc_name, CP, VM):
@@ -126,7 +125,7 @@ class CarController():
     self.brake_last = rate_limit(pre_limit_brake, self.brake_last, -2., DT_CTRL)
 
     # vehicle hud display, wait for one update from 10Hz 0x304 msg
-    if hud_show_lanes:
+    if hud_show_lanes and CS.lkMode:
       hud_lanes = 1
     else:
       hud_lanes = 0
@@ -147,7 +146,7 @@ class CarController():
     # steer torque is converted back to CAN reference (positive when steering right)
     apply_steer = int(interp(-actuators.steer * P.STEER_MAX, P.STEER_LOOKUP_BP, P.STEER_LOOKUP_V))
 
-    lkas_active = enabled and not CS.steer_not_allowed
+    lkas_active = enabled and not CS.steer_not_allowed and CS.lkMode
 
     # Send CAN commands.
     can_sends = []
@@ -239,7 +238,7 @@ class CarController():
             can_sends.append(create_gas_command(self.packer, apply_gas, idx))
 
     hud = HUDData(int(pcm_accel), int(round(hud_v_cruise)), hud_car,
-                  hud_lanes, fcw_display, acc_alert, steer_required)
+                  hud_lanes, fcw_display, acc_alert, steer_required, CS.lkMode)
 
     # Send dashboard UI commands.
     if (frame % 10) == 0:
