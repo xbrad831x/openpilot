0f78e9ef1cc3d37b0cd1d4837384652da0db6005
Casey Francis <@CFranHonda>
2021-09-18 09:17:04 -0700

    Revert "Update launch_chffrplus.sh"
    This reverts commit 47fbb7c8781fa11a5c0a456ff707fc17c4c7b1bd.

--- launch_chffrplus.sh
+++ launch_chffrplus.sh
@@ -9,16 +9,6 @@ source "$BASEDIR/launch_env.sh"
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 function two_init {
-  mount -o remount,rw /system
-  if [ ! -f /ONEPLUS ] && ! $(grep -q "letv" /proc/cmdline); then
-    sed -i -e 's#/dev/input/event1#/dev/input/event2#g' ~/.bash_profile
-    touch /ONEPLUS
-  else
-    if [ ! -f /LEECO ]; then
-      touch /LEECO
-    fi
-  fi
-  mount -o remount,r /system
 
   # set IO scheduler
   setprop sys.io.scheduler noop
@@ -76,9 +66,6 @@ function two_init {
 
   # USB traffic needs realtime handling on cpu 3
   [ -d "/proc/irq/733" ] && echo 3 > /proc/irq/733/smp_affinity_list
-  if [ -f /ONEPLUS ]; then
-    [ -d "/proc/irq/736" ] && echo 3 > /proc/irq/736/smp_affinity_list # USB for OP3T
-  fi
 
   # GPU and camera get cpu 2
   CAM_IRQS="177 178 179 180 181 182 183 184 185 186 192"
@@ -102,33 +89,19 @@ function two_init {
   wpa_cli IFNAME=wlan0 SCAN
 
   # Check for NEOS update
-  if [ -f /LEECO ]; then
-    if [ $(< /VERSION) != "$REQUIRED_NEOS_VERSION" ]; then
-      if [ -f "$DIR/scripts/continue.sh" ]; then
-        cp "$DIR/scripts/continue.sh" "/data/data/com.termux/files/continue.sh"
-      fi
-
-      if [ ! -f "$BASEDIR/prebuilt" ]; then
-        # Clean old build products, but preserve the scons cache
-        cd $DIR
-        git clean -xdf
-        git submodule foreach --recursive git clean -xdf
-      fi
-
-      "$DIR/installer/updater/updater" "file://$DIR/installer/updater/update.json"
+  if [ $(< /VERSION) != "$REQUIRED_NEOS_VERSION" ]; then
+    if [ -f "$DIR/scripts/continue.sh" ]; then
+      cp "$DIR/scripts/continue.sh" "/data/data/com.termux/files/continue.sh"
     fi
-  fi
 
-  # One-time fix for a subset of OP3T with gyro orientation offsets.
-  # Remove and regenerate qcom sensor registry. Only done on OP3T mainboards.
-  # Performed exactly once. The old registry is preserved just-in-case, and
-  # doubles as a flag denoting we've already done the reset.
-  if [ ! -f /ONEPLUS ] && [ ! -f "/persist/comma/op3t-sns-reg-backup" ]; then
-    echo "Performing OP3T sensor registry reset"
-    mv /persist/sensors/sns.reg /persist/comma/op3t-sns-reg-backup &&
-      rm -f /persist/sensors/sensors_settings /persist/sensors/error_log /persist/sensors/gyro_sensitity_cal &&
-      echo "restart" > /sys/kernel/debug/msm_subsys/slpi &&
-      sleep 5  # Give Android sensor subsystem a moment to recover
+    if [ ! -f "$BASEDIR/prebuilt" ]; then
+      # Clean old build products, but preserve the scons cache
+      cd $DIR
+      git clean -xdf
+      git submodule foreach --recursive git clean -xdf
+    fi
+
+    "$DIR/installer/updater/updater" "file://$DIR/installer/updater/update.json"
   fi
 }
 
