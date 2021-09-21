5970eb61cd9ea7f69616ffa10ad678cccd0b6293
Casey Francis <@CFranHonda>
2021-09-20 17:23:59 -0700

    Revert "add LQR to RDX"
    This reverts commit 156b6414425e230dc5279a0f738cfc29297df0aa.

--- selfdrive/car/honda/interface.py
+++ selfdrive/car/honda/interface.py
@@ -280,17 +280,7 @@ class CarInterface(CarInterfaceBase):
       ret.centerToFront = ret.wheelbase * 0.41
       ret.steerRatio = 11.95  # as spec
       ret.lateralParams.torqueBP, ret.lateralParams.torqueV = [[0, 3840], [0, 3840]]
-      ret.lateralTuning.init('lqr')
-        
-      ret.lateralTuning.lqr.scale = 1200.0
-      ret.lateralTuning.lqr.ki = 0.18
-        
-      ret.lateralTuning.lqr.a = [0., 1., -0.22619643, 1.21822268]
-      ret.lateralTuning.lqr.b = [-1.92006585e-04, 3.95603032e-05]
-      ret.lateralTuning.lqr.c = [1., 0.]
-      ret.lateralTuning.lqr.k = [-110.73572306, 451.22718255]
-      ret.lateralTuning.lqr.l = [0.3233671, 0.3185757]
-      ret.lateralTuning.lqr.dcGain = 0.002237852961363602
+      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.6], [0.18]]
       tire_stiffness_factor = 0.677
 
     elif candidate == CAR.ODYSSEY:
