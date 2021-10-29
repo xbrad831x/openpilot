80b9f021f7822aaa833b34f00bf428533b8d271d
KRKeegan <kevin@krkeegan.com>
2021-10-29 16:36:00 -0700

    :fuelpump: Add Toggle to Enable Use of Gas Pedal While Openpilot is Engaged
    USE AT YOUR OWN RISK

--- selfdrive/car/interfaces.py
+++ selfdrive/car/interfaces.py
@@ -5,6 +5,7 @@ from typing import Dict, Tuple, List
 
 from cereal import car
 from common.kalman.simple_kalman import KF1D
+from common.params import Params
 from common.realtime import DT_CTRL
 from selfdrive.car import gen_empty_fingerprint
 from selfdrive.config import Conversions as CV
@@ -44,6 +45,10 @@ class CarInterfaceBase(ABC):
     if CarController is not None:
       self.CC = CarController(self.cp.dbc_name, CP, self.VM)
 
+    # KRKeegan Option to Enable Gas on Cruise
+    params = Params()
+    self.enable_gas_on_cruise = params.get_bool("EnableGasOnCruise")
+
   @staticmethod
   def get_pid_accel_limits(CP, current_speed, cruise_speed):
     return ACCEL_MIN, ACCEL_MAX
@@ -73,6 +78,9 @@ class CarInterfaceBase(ABC):
     ret = car.CarParams.new_message()
     ret.carFingerprint = candidate
     ret.unsafeMode = 0  # see panda/board/safety_declarations.h for allowed values
+    params = Params()
+    if params.get_bool("EnableGasOnCruise"):
+      ret.unsafeMode = ret.unsafeMode | 1
 
     # standard ALC params
     ret.steerControlType = car.CarParams.SteerControlType.torque
@@ -125,7 +133,7 @@ class CarInterfaceBase(ABC):
       events.add(EventName.wrongCarMode)
     if cs_out.espDisabled:
       events.add(EventName.espDisabled)
-    if cs_out.gasPressed:
+    if cs_out.gasPressed and not self.enable_gas_on_cruise:
       events.add(EventName.gasPressed)
     if cs_out.stockFcw:
       events.add(EventName.stockFcw)
@@ -154,7 +162,7 @@ class CarInterfaceBase(ABC):
       events.add(EventName.steerUnavailable)
 
     # Disable on rising edge of gas or brake. Also disable on brake when speed > 0.
-    if (cs_out.gasPressed and not self.CS.out.gasPressed) or \
+    if (not self.enable_gas_on_cruise and cs_out.gasPressed and not self.CS.out.gasPressed) or \
        (cs_out.brakePressed and (not self.CS.out.brakePressed or not cs_out.standstill)):
       events.add(EventName.pedalPressed)
 
--- selfdrive/common/params.cc
+++ selfdrive/common/params.cc
@@ -104,6 +104,7 @@ std::unordered_map<std::string, uint32_t> keys = {
     {"DoUninstall", CLEAR_ON_MANAGER_START},
     {"EnableWideCamera", CLEAR_ON_MANAGER_START},
     {"EndToEndToggle", PERSISTENT},
+    {"EnableGasOnCruise", PERSISTENT},
     {"ForcePowerDown", CLEAR_ON_MANAGER_START},
     {"GitBranch", PERSISTENT},
     {"GitCommit", PERSISTENT},
