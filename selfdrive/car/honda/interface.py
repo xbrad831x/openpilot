f93509003f2eabb473bf89a7608760be69df3860
Casey Francis <>
2022-06-10 20:49:41 -0700

    remove LQR from honda, sorry guys

--- selfdrive/car/honda/interface.py
+++ selfdrive/car/honda/interface.py
@@ -90,26 +90,8 @@ class CarInterface(CarInterfaceBase):
       ret.centerToFront = CivicParams.CENTER_TO_FRONT
       ret.steerRatio = 15.38  # 10.93 is end-to-end spec
       if eps_modified:
-        # stock request input values:     0x0000, 0x00DE, 0x014D, 0x01EF, 0x0290, 0x0377, 0x0454, 0x0610, 0x06EE
-        # stock request output values:    0x0000, 0x0917, 0x0DC5, 0x1017, 0x119F, 0x140B, 0x1680, 0x1680, 0x1680
-        # modified request output values: 0x0000, 0x0917, 0x0DC5, 0x1017, 0x119F, 0x140B, 0x1680, 0x2880, 0x3180
-        # stock filter output values:     0x009F, 0x0108, 0x0108, 0x0108, 0x0108, 0x0108, 0x0108, 0x0108, 0x0108
-        # modified filter output values:  0x009F, 0x0108, 0x0108, 0x0108, 0x0108, 0x0108, 0x0108, 0x0400, 0x0480
-        # note: max request allowed is 4096, but request is capped at 3840 in firmware, so modifications result in 2x max
         ret.lateralParams.torqueBP, ret.lateralParams.torqueV = [[0, 2560, 8000], [0, 2560, 3840]]
-        # Use LQR tune if param is set
-        if Params().get_bool('LqrTune'):
-          ret.lateralTuning.init('lqr')
-          ret.lateralTuning.lqr.scale = 1200.0
-          ret.lateralTuning.lqr.ki = 0.02
-          ret.lateralTuning.lqr.a = [0., 1., -0.22619643, 1.21822268]
-          ret.lateralTuning.lqr.b = [-1.92006585e-04, 3.95603032e-05]
-          ret.lateralTuning.lqr.c = [1., 0.]
-          ret.lateralTuning.lqr.k = [-110.73572306, 451.22718255]
-          ret.lateralTuning.lqr.l = [0.3233671, 0.3185757]
-          ret.lateralTuning.lqr.dcGain = 0.002737852961363602
-        else:
-          ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.3], [0.1]]
+        ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.3], [0.1]]
       else:
         ret.lateralParams.torqueBP, ret.lateralParams.torqueV = [[0, 2560], [0, 2560]]
         ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[1.1], [0.33]]
@@ -123,21 +105,9 @@ class CarInterface(CarInterfaceBase):
       ret.steerRatio = 15.38  # 10.93 is end-to-end spec
       if eps_modified:
         ret.lateralParams.torqueBP, ret.lateralParams.torqueV = [[0, 2564, 8000], [0, 2564, 3840]]
-        # Use LQR tune if param is set
-        if Params().get_bool('LqrTune'):
-          ret.lateralTuning.init('lqr')
-          ret.lateralTuning.lqr.scale = 1200.0
-          ret.lateralTuning.lqr.ki = 0.1
-          ret.lateralTuning.lqr.a = [0., 1., -0.22619643, 1.21822268]
-          ret.lateralTuning.lqr.b = [-1.92006585e-04, 3.95603032e-05]
-          ret.lateralTuning.lqr.c = [1., 0.]
-          ret.lateralTuning.lqr.k = [-110.73572306, 451.22718255]
-          ret.lateralTuning.lqr.l = [0.3233671, 0.3185757]
-          ret.lateralTuning.lqr.dcGain = 0.002237852961363602
-        else:
-          ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.36], [0.108]] #minus 10% from 0.4, 0.12
+        ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.36], [0.108]] #minus 10% from 0.4, 0.12
       else:
-        ret.lateralParams.torqueBP, ret.lateralParams.torqueV = [[0, 4096], [0, 4096]]  # TODO: determine if there is a dead zone at the top end
+        ret.lateralParams.torqueBP, ret.lateralParams.torqueV = [[0, 4096], [0, 4096]]
         ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.8], [0.24]]
       tire_stiffness_factor = 1.
 
@@ -147,19 +117,11 @@ class CarInterface(CarInterfaceBase):
       ret.wheelbase = 2.83
       ret.centerToFront = ret.wheelbase * 0.39
       ret.steerRatio = 16.33  # 11.82 is spec end-to-end
-      ret.lateralParams.torqueBP, ret.lateralParams.torqueV = [[0, 4096], [0, 4096]]  # TODO: determine if there is a dead zone at the top end
-      # Use LQR tune if param is set
-      if Params().get_bool('LqrTune'):
-        ret.lateralTuning.init('lqr')
-        ret.lateralTuning.lqr.scale = 1200.0
-        ret.lateralTuning.lqr.ki = 0.1
-        ret.lateralTuning.lqr.a = [0., 1., -0.22619643, 1.21822268]
-        ret.lateralTuning.lqr.b = [-1.92006585e-04, 3.95603032e-05]
-        ret.lateralTuning.lqr.c = [1., 0.]
-        ret.lateralTuning.lqr.k = [-110.73572306, 451.22718255]
-        ret.lateralTuning.lqr.l = [0.3233671, 0.3185757]
-        ret.lateralTuning.lqr.dcGain = 0.002237852961363602
+      if eps_modified:
+        ret.lateralParams.torqueBP, ret.lateralParams.torqueV = [[0, 4096], [0, 4096]]
+        ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.3], [0.09]]
       else:
+        ret.lateralParams.torqueBP, ret.lateralParams.torqueV = [[0, 4096], [0, 4096]]
         ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.6], [0.18]]
       tire_stiffness_factor = 0.8467
         
@@ -191,23 +153,8 @@ class CarInterface(CarInterfaceBase):
       ret.centerToFront = ret.wheelbase * 0.41
       ret.steerRatio = 16.0  # 12.3 is spec end-to-end
       if eps_modified:
-        # stock request input values:     0x0000, 0x00DB, 0x01BB, 0x0296, 0x0377, 0x0454, 0x0532, 0x0610, 0x067F
-        # stock request output values:    0x0000, 0x0500, 0x0A15, 0x0E6D, 0x1100, 0x1200, 0x129A, 0x134D, 0x1400
-        # modified request output values: 0x0000, 0x0500, 0x0A15, 0x0E6D, 0x1100, 0x1200, 0x1ACD, 0x239A, 0x2800
         ret.lateralParams.torqueBP, ret.lateralParams.torqueV = [[0, 2560, 10000], [0, 2560, 3840]]
-        # Use LQR tune if param is set
-        if Params().get_bool('LqrTune'):
-          ret.lateralTuning.init('lqr')
-          ret.lateralTuning.lqr.scale = 1200.0
-          ret.lateralTuning.lqr.ki = 0.1
-          ret.lateralTuning.lqr.a = [0., 1., -0.22619643, 1.21822268]
-          ret.lateralTuning.lqr.b = [-1.92006585e-04, 3.95603032e-05]
-          ret.lateralTuning.lqr.c = [1., 0.]
-          ret.lateralTuning.lqr.k = [-110.73572306, 451.22718255]
-          ret.lateralTuning.lqr.l = [0.3233671, 0.3185757]
-          ret.lateralTuning.lqr.dcGain = 0.002237852961363602
-        else:
-          ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.21], [0.07]]
+        ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.21], [0.07]]
       else:
         ret.lateralParams.torqueBP, ret.lateralParams.torqueV = [[0, 3840], [0, 3840]]
         ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.64], [0.192]]
@@ -274,20 +221,8 @@ class CarInterface(CarInterfaceBase):
       ret.centerToFront = ret.wheelbase * 0.41
       ret.steerRatio = 16.0 #11.95 is spec
       ret.lateralParams.torqueBP, ret.lateralParams.torqueV = [[0, 3840], [0, 3840]]
-      # Use LQR tune if param is set
-      if Params().get_bool('LqrTune'):
-        ret.lateralTuning.init('lqr')
-        ret.lateralTuning.lqr.scale = 1200.0
-        ret.lateralTuning.lqr.ki = 0.1
-        ret.lateralTuning.lqr.a = [0., 1., -0.22619643, 1.21822268]
-        ret.lateralTuning.lqr.b = [-1.92006585e-04, 3.95603032e-05]
-        ret.lateralTuning.lqr.c = [1., 0.]
-        ret.lateralTuning.lqr.k = [-110.73572306, 451.22718255]
-        ret.lateralTuning.lqr.l = [0.3233671, 0.3185757]
-        ret.lateralTuning.lqr.dcGain = 0.002237852961363602
-      else:
-        ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.2], [0.06]]
-        ret.lateralTuning.pid.kf = 0.00007818594
+      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.2], [0.06]]
+      ret.lateralTuning.pid.kf = 0.00007818594
       tire_stiffness_factor = 0.677
 
 
