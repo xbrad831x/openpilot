e4f0cdabbd50a61d068f30a9c7b54191d9291856
cfranyota <46506059+cfranyota@users.noreply.github.com>
2022-06-22 10:58:04 -0700

    Typo

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -571,7 +571,7 @@ class Controls:
                                                                              lat_plan.curvatureRates)
       actuators.steer, actuators.steeringAngleDeg, lac_log = self.LaC.update(CC.latActive, CS, self.VM, params,
                                                                              self.last_actuators, desired_curvature,
-                                                                             desired_curvature_rate, self.sm['liveLocationKalman be'], llk)
+                                                                             desired_curvature_rate, self.sm['liveLocationKalman'], llk)
     else:
       lac_log = log.ControlsState.LateralDebugState.new_message()
       if self.sm.rcv_frame['testJoystick'] > 0:
