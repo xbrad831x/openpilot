0ec5f5dab784f27369bf90616b3a6593ec55d054
Casey Francis <>
2022-07-03 14:32:08 -0700

    increase for sluggish follow

--- selfdrive/controls/lib/longitudinal_planner.py
+++ selfdrive/controls/lib/longitudinal_planner.py
@@ -21,7 +21,7 @@ from selfdrive.swaglog import cloudlog
 LON_MPC_STEP = 0.2  # first step is 0.2s
 AWARENESS_DECEL = -0.2  # car smoothly decel at .2m/s^2 when user is distracted
 A_CRUISE_MIN = -1.2
-A_CRUISE_MAX_VALS = [1.6, 1.4, 1.0, 0.6, 0.3]  # Sets the limits of the planner accel, PID may exceed
+A_CRUISE_MAX_VALS = [1.6, 1.4, 1.2, 0.8, 0.5]  # Sets the limits of the planner accel, PID may exceed
 A_CRUISE_MAX_BP = [0., 5., 10., 20., 55.]
 
 
