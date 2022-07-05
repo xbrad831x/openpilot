22712ed4dab30ee6a496cb6dcfd69f71588a6607
Casey Francis <>
2022-07-04 20:28:31 -0700

    only for bosch candidates

--- selfdrive/car/honda/interface.py
+++ selfdrive/car/honda/interface.py
@@ -72,6 +72,7 @@ class CarInterface(CarInterfaceBase):
       ret.longitudinalTuning.kpV = [0.25]
       ret.longitudinalTuning.kiV = [0.05]
       ret.longitudinalActuatorDelayUpperBound = 0.5 # s
+      ret.stoppingDecelRate = 1.0 # brake_travel/s while trying to stop
     else:
       # default longitudinal tuning for all hondas
       ret.longitudinalTuning.kpBP = [0., 5., 35.]
@@ -369,7 +370,6 @@ class CarInterface(CarInterfaceBase):
     ret.steerActuatorDelay = 0.1
     ret.steerRateCost = 0.5
     ret.steerLimitTimer = 0.8
-    ret.stoppingDecelRate = 1.0 # brake_travel/s while trying to stop
 
     return ret
 
