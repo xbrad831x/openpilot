65e1943284276926ac4ddbaaef9bac7cebdef88b
Casey Francis <>
2022-07-08 10:05:52 -0700

    revert this..
    Update car.capnp

--- selfdrive/car/toyota/tunes.py
+++ selfdrive/car/toyota/tunes.py
@@ -47,9 +47,9 @@ def set_long_tune(tune, name):
     tune.deadzoneBP = [0., 9.]
     tune.deadzoneV = [0., .15]
     tune.kpBP = [0., 5., 35.]
-    tune.kiBP = [0., 5., 35.]
-    tune.kpV = [2.95, 2.4, 1.5]
-    tune.kiV = [0.28, 18/35, 0.36]
+    tune.kiBP = [0., 35.]
+    tune.kpV = [3.6, 2.4, 1.5]
+    tune.kiV = [0.54, 0.36]
   else:
     raise NotImplementedError('This longitudinal tune does not exist')
 
